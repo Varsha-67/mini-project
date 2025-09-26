3d5985dff1980dfac3d66704e78a8cb43f9907f9
Varsha <167610228+Varsha-67@users.noreply.github.com>
2025-09-26 17:31:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Small JS interaction (button effect)
+document.querySelector(".green-btn").addEventListener("click", () => {
+  alert("Innovation journey started!");
+});
+
+document.querySelector(".demo-btn").addEventListener("click", () => {
+  alert("Playing demo...");
+});
